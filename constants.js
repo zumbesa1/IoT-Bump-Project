5ddf052b6e5c6499f4b2c3a7e987c930b6007ccc
reddiric <reddiric@students.zhaw.ch>
2020-10-31 11:22:48 +0100

    add device 2

--- constants.js
+++ constants.js
@@ -1,4 +1,6 @@
 module.exports = {
     device_id_1: "e00fce685260d30b650b6661",
-    access_token_1: "07b08231f18daacf6ee1e3f3b3b60e5e2415b18b",
+    access_token_1: "07b08231f18daacf6ee1e3f3b3b60e5e2415b18b"
+    device_id_2: "e00fce6855c88e743e4ef870",
+    access_token_2: "d3169335d7203eadaef7cb8bc0c2e22933d2015b",
 }
